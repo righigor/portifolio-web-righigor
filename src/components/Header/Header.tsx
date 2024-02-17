c7f8d71f820828e10bd7df04793cd75837396541
Igor Righi <cruigor@gmail.com>
2024-02-17 14:00:11 -0300

    finalizando o drop menu na versao mobile

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,13 @@
-/* eslint-disable max-len */
-/* eslint-disable react/jsx-indent */
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import style from './Header.module.css';
-import curriculo from '../../resume/igor-righi-curriculo.pdf';
+import DropMenu from './DropMenu';
+import NavMenu from './NavMenu';
 
 function Header() {
-  const [openMenu] = useState(false);
   const [widthScreen, setWidhScreen] = useState({
     largura: window.innerWidth,
   });
-  console.log(widthScreen);
   const atualizarTamanhoDaTela = () => {
     setWidhScreen({
       largura: window.innerWidth,
@@ -18,21 +15,11 @@ function Header() {
   };
 
   useEffect(() => {
-    // Adiciona um listener de redimensionamento quando o componente é montado
     window.addEventListener('resize', atualizarTamanhoDaTela);
-
-    // Remove o listener de redimensionamento quando o componente é desmontado
     return () => {
       window.removeEventListener('resize', atualizarTamanhoDaTela);
     };
   }, []);
-
-  const handleBtnDownload = () => {
-    const link = document.createElement('a');
-    link.href = curriculo;
-    link.download = 'curriculo-igor-righi';
-    link.click();
-  };
 
   return (
     <div className={ style.headerContainer }>
@@ -41,45 +28,9 @@ function Header() {
           <span>Igor Righi</span>
         </div>
       </Link>
-{/*
-      { widthScreen.largura <= 576
-        ? <div className={ style.mobileMenuButton } onClick={ () => setOpenMenu(!openMenu) }>
-            <FontAwesomeIcon icon={ openMenu ? faTimes : faBars } />
-          </div>
-        : undefined} */}
-      <div className={ `${style.linkContainer} ${openMenu ? style.mobileMenuOpen : ''}` }>
-        <Link to="about-me" className={ style.linkStyle }>
-          <span>
-            Sobre Mim
-          </span>
-        </Link>
-
-        <Link to="skills" className={ style.linkStyle }>
-          <span>Habilidades</span>
-        </Link>
-
-        <Link to="projects" className={ style.linkStyle }>
-          <span>Projetos</span>
-        </Link>
-
-        <Link to="education" className={ style.linkStyle }>
-          <span>Formação</span>
-        </Link>
-
-        <Link to="contact" className={ style.linkStyle }>
-          <span>Contato</span>
-        </Link>
-      </div>
-
-      <div>
-        <button
-          title="Faça download do meu currículo"
-          className={ style.btn }
-          onClick={ handleBtnDownload }
-        >
-          Meu Currículo
-        </button>
-      </div>
+      {(widthScreen.largura > 768)
+        ? <NavMenu />
+        : <DropMenu />}
     </div>
   );
 }
