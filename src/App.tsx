3b586996ef234f51f6afc11876b3f1d7d4257852
igor <cruigor@gmail.com>
2024-06-26 16:53:42 -0300

    maintanance page

--- src/App.tsx
+++ src/App.tsx
@@ -11,23 +11,25 @@ import Contact from './pages/Contact/Contact';
 import CourseDetailed from './pages/CourseDetailed/CourseDetailed';
 import Institutions from './pages/Institutions/Institutions';
 import ProjectsDetailed from './pages/ProjectsDetailed/ProjectsDetailed';
+import MaintenancePage from './pages/MaintanancePage/MaintanancePage';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={ <Layout /> }>
-        <Route index element={ <Home /> } />
-        <Route path="about-me" element={ <AboutMe /> } />
-        <Route path="skills" element={ <Skills /> } />
-        <Route path="projects" element={ <Projects /> } />
-        <Route path="projects/:id" element={ <ProjectsDetailed /> } />
-        <Route path="education" element={ <Education /> } />
-        <Route path="education/:id" element={ <CourseDetailed /> } />
-        <Route path="institutions/:id" element={ <Institutions /> } />
-        <Route path="contact" element={ <Contact /> } />
-        <Route path="/*" element={ <NotFound /> } />
-      </Route>
-    </Routes>
+    // <Routes>
+    //   <Route path="/" element={ <Layout /> }>
+    //     <Route index element={ <Home /> } />
+    //     <Route path="about-me" element={ <AboutMe /> } />
+    //     <Route path="skills" element={ <Skills /> } />
+    //     <Route path="projects" element={ <Projects /> } />
+    //     <Route path="projects/:id" element={ <ProjectsDetailed /> } />
+    //     <Route path="education" element={ <Education /> } />
+    //     <Route path="education/:id" element={ <CourseDetailed /> } />
+    //     <Route path="institutions/:id" element={ <Institutions /> } />
+    //     <Route path="contact" element={ <Contact /> } />
+    //     <Route path="/*" element={ <NotFound /> } />
+    //   </Route>
+    // </Routes>
+    <MaintenancePage />
   );
 }
 
